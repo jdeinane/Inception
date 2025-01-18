5858decd041983b650ca17f4ee666cde619e9ed1
jubaldo <jubaldo@inception.jubaldo.virtualbox.org>
2025-01-18 17:55:19 +0100

    mariadb functionnal

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,18 +1,19 @@
-#!/bin/bash
-#set -eux
+#!/bin/sh
 
-service mysql start;
+service mariadb start;
 
-# log into MariaDB as root and create database and the user
+sleep 1
+
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+
 mysql -e "FLUSH PRIVILEGES;"
 
 mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
-#mysqladmin -u root shutdown
-exec mysqld_safe
 
-#print status
-echo "MariaDB database and user were created successfully! "
+exec mysqld_safe
